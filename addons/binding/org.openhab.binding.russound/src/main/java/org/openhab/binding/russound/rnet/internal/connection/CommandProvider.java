89a8456e845441d76529718d8e7540328c3820c2
craigh <craigh@quailholdings.com>
2017-05-08 17:12:23 -0700

    added the classes....

---
+++ addons/binding/org.openhab.binding.russound/src/main/java/org/openhab/binding/russound/rnet/internal/connection/CommandProvider.java
@@ -0,0 +1,6 @@
+package org.openhab.binding.russound.rnet.internal.connection;
+
+public interface CommandProvider {
+
+    public byte[] getCommand();
+}
---
+++ addons/binding/org.openhab.binding.russound/src/main/java/org/openhab/binding/russound/rnet/internal/connection/ConnectionStateListener.java
@@ -0,0 +1,6 @@
+package org.openhab.binding.russound.rnet.internal.connection;
+
+public interface ConnectionStateListener {
+
+    public void isConnected(boolean value);
+}
